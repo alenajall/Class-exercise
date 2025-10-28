30993a7d2a3c4e2845cba94a5a10f82fd283d8db
Alhaji jalloh <alenajall10@gmail.com>
2025-10-28 04:58:36 -0700

    Create exercise for tuples.py

---
+++ exercise for tuples.py
@@ -0,0 +1,14 @@
+# Given tuple
+session_times = ("08:30", "11:30", "14:30", "17:30", "20:30")
+
+# Print first session
+print("First session:", session_times[0])
+
+# Print last session
+print("Last session:", session_times[-1])
+
+# Print morning session (first two)
+print("Morning sessions:", session_times[0:2])
+
+# Print reversed times
+print("Reversed times:", session_times[::-1])
